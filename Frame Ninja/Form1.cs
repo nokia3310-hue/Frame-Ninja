71e8d24f2829d8fd2e3757510fb9f34b38cedfd8
samuelus3 <samuelus@outlook.de>
2024-08-01 15:52:55 +0100

    final commit for today

--- Frame Ninja/Form1.cs
+++ Frame Ninja/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Frame_Ninja
@@ -14,8 +15,14 @@ namespace Frame_Ninja
     public partial class Form1 : Form
     {
         public static Fruit[] Mainfruit = new Fruit[20];
+        
         public static CursorTrailNodes[] Cursortrail = new CursorTrailNodes[100];
-        public Point[] mouseTrack = new Point[20];
+        public static Point[] mouseTrack = new Point[20];
+        public static Fruit[] Mainfred = new Fruit[5];
+        public static int visiblenodes = 100;
+
+
+        public static System.Timers.Timer _delayTimer;
         public Form1()
         {
             InitializeComponent();
@@ -24,8 +31,9 @@ namespace Frame_Ninja
 
             for (int i = 0; i < Mainfruit.Length; i++)
             {
+                
                 Mainfruit[i] = new Fruit(this, i);
-                Mainfruit[i].Show();
+                
             }
             for (int i = 0; i < Cursortrail.Length; i++)
             {
@@ -33,26 +41,47 @@ namespace Frame_Ninja
                 Cursortrail[i].Show();
             }
 
+            _delayTimer = new System.Timers.Timer(); // was auch immer das macht
+            _delayTimer.Interval = 10; 
+            _delayTimer.Elapsed += ExecuteFunction; 
+            _delayTimer.AutoReset = true; 
 
-            Thread vt = new Thread(new ThreadStart(velocitycycle));
-            vt.Start();
+           
+            _delayTimer.Start();
+            
         }
 
-        private void velocitycycle()
+        public static double velocity = 0;
+
+        private static void ExecuteFunction(object sender, ElapsedEventArgs e)
         {
-            while(true)
-            {
+
+
+
+            double vel = 0;
+            
+            
                 for(int i = mouseTrack.Length-1; i > 0; i--)
                 {
                     
                     mouseTrack[i] = mouseTrack[i - 1];
                     mouseTrack[0] = Cursor.Position;
-
-                    Mainfruit[i].Location = mouseTrack[i];
+                
+                    
                 }
 
-                Thread.Sleep(100);
+                for(int i = 0; i < mouseTrack.Length-2; i++)
+            {
+                vel += Math.Sqrt(Math.Pow(mouseTrack[i+1].X - mouseTrack[i].X, 2) + Math.Pow(mouseTrack[i+1].Y - mouseTrack[i].Y, 2));
             }
+            velocity = vel;
+
+            Console.WriteLine("vel " + vel);
+            double n = velocity / 10;
+            visiblenodes = (int)(n > Cursortrail.Length-1 ? Cursortrail.Length-1 : n);
+
+
+
         }
 
         private bool mouseisdown;
